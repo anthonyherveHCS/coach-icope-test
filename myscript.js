d2c621de41d9256a0d400e26b592d7f1fb3849f7
anthonyherveHCS <herve.a@fr.ibm.com>
2026-01-30 10:20:30 +0100

    Add icope test for Watson Orchestrate integration

--- myscript.js
+++ myscript.js
@@ -1,4 +1,24 @@
+<script>
+  window.wxOConfiguration = {
+    orchestrationID: '713c783d9a507a53135fe6793c24249a_6d322ff4-c015-4dba-8fc5-c1e5c6fdfe64',
+    hostURL: 'https://us-south.watson-orchestrate.cloud.ibm.com',
+    rootElementID: "root",
+    deploymentPlatform: "ibmcloud",
+    crn: 'crn:v1:bluemix:public:watsonx-orchestrate:us-south:a/713c783d9a507a53135fe6793c24249a:6d322ff4-c015-4dba-8fc5-c1e5c6fdfe64::',
+    chatOptions: {
+        agentId: 'e96d6c3d-5232-4389-9f37-c86af9546efa', 
+    }
+  };
+  setTimeout(function () {
+    const script = document.createElement('script');
+    script.src = `${window.wxOConfiguration.hostURL}/wxochat/wxoLoader.js?embed=true`;
+    script.addEventListener('load', function () {
+        wxoLoader.init();
+    });
+    document.head.appendChild(script);
+  }, 0);
 
+/*    
 window.wxOConfiguration = {
     orchestrationID: 
 'f665a69257a9fbe8b8bf0f77bcee465e_cb18e62f-3b09-4337-a728-3693e9a8f16f',
@@ -22,3 +42,4 @@ setTimeout(function () {
     });
     document.head.appendChild(script);
 }, 0);
+*/
